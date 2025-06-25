85fadfe56d6f8d5e03300e5c052ae45deefa2c8d
Brady Vo <bradylvo@gmail.com>
2025-06-25 15:34:32 -0500

    Configure Security with SecurityFilterChain bean

--- src/main/java/com/gymloggingapp/gymloggingapp/config/SecurityConfig.java
+++ src/main/java/com/gymloggingapp/gymloggingapp/config/SecurityConfig.java
@@ -1,4 +1,41 @@
 package com.gymloggingapp.gymloggingapp.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+    return httpSecurity.csrf(csrf-> csrf.disable())
+            .sessionManagement(session->
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(auth->
+                    auth.requestMatchers("/api/auth/**").permitAll()
+                    .anyRequest().authenticated())
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+            .exceptionHandling(exception->
+                    exception.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                            .accessDeniedHandler((request,
+                                                   response,
+                                                   accessDeniedException) -> {
+                                response.setStatus(HttpStatus.FORBIDDEN.value());
+                                response.setContentType("application/json");
+                                response.getWriter().write("{\"error\": \"Access Denied\"}");
+                            })
+                            )
+            .build();
+    }
 }
