58a15f7673dcc0aef7e7ed80e3c933c97473b84f
Brady Vo <bradylvo@gmail.com>
2025-06-27 16:12:02 -0500

    Manually create AuthenticationManager Bean

--- src/main/java/com/gymloggingapp/gymloggingapp/config/AppConfig.java
+++ src/main/java/com/gymloggingapp/gymloggingapp/config/AppConfig.java
@@ -5,8 +5,10 @@ import com.gymloggingapp.gymloggingapp.Repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -28,4 +30,12 @@ public class AppConfig {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws  Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+
 }
