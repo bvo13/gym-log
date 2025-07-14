1df4e6415c66b1c5198ed450b20009dfa25990d3
Brady Vo <bradylvo@gmail.com>
2025-07-14 14:11:43 -0500

    Add CORS to filter chain

--- src/main/java/com/gymloggingapp/gymloggingapp/config/SecurityConfig.java
+++ src/main/java/com/gymloggingapp/gymloggingapp/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -23,7 +24,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
-    return httpSecurity.csrf(csrf-> csrf.disable())
+    return httpSecurity.cors(Customizer.withDefaults())
+        .csrf(csrf-> csrf.disable())
             .sessionManagement(session->
                     session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth->
